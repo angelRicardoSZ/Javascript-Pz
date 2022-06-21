5776c2482f77c9232c6c6ac9435504face3a5ded
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-06-21 08:33:26 -0500

    webpack - Loaders and plugins babel, html in webpack and loaders for css and preprocessors

--- webpack/First project/webpack.config.js
+++ webpack/First project/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: "./src/index.js",
     output: {
@@ -8,5 +9,32 @@ module.exports = {
     },
     resolve: {
         extensions: [".js"]
-    }
+    },
+    module:{
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css|.styl$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, "css-loader",
+                    "stylus-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template:"./public/index.html",
+            filename: "./index.html"
+
+        }),
+        new MiniCssExtractPlugin(),
+    ]
 }
